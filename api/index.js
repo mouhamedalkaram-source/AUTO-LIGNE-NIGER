4fe92a59633953021423c9ea5482c8a8c6a894fb
mouhamedalkaram-source <mouhamedalkaram@gmail.com>
2026-07-02 02:45:02 +0100

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,1 +1,12 @@
-module.exports = require("../server-core");
+const handler = require("../server-core");
+
+module.exports = (req, res) => {
+  const url = new URL(req.url, "http://localhost");
+  const path = String(url.searchParams.get("path") || "").replace(/^\/+/, "");
+  const nextParams = new URLSearchParams(url.searchParams);
+  nextParams.delete("path");
+  const query = nextParams.toString();
+
+  req.url = `/api${path ? `/${path}` : ""}${query ? `?${query}` : ""}`;
+  return handler(req, res);
+};
